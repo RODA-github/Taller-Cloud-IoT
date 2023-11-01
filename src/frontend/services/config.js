91c7b65700d242d1cccc8992d37208d43e2bc173
Sebas-Ar <sebastian.arias@mercadolibre.com.co>
2023-11-01 02:42:37 -0500

    Update template for workshop

--- src/frontend/services/config.js
+++ src/frontend/services/config.js
@@ -1,8 +1,1 @@
-const AWS = require('aws-sdk')
 
-AWS.config.update({
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
-})
-
-export default AWS
